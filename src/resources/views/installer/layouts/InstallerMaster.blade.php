16741bf1e3fce0462844a8b6e25bd615e7213d47
spruko <dev@team.spruko.com>
2024-04-02 16:57:32 +0530

    final Commit

--- src/resources/views/installer/layouts/InstallerMaster.blade.php
+++ src/resources/views/installer/layouts/InstallerMaster.blade.php
@@ -6,9 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@if (trim($__env->yieldContent('template_title')))@yield('template_title') | @endif @if (trim($__env->yieldContent('title')))@yield('title')@endif</title>
-        <link rel="icon" type="image/png" href="{{ asset('installer/img/favicon/favicon-16x16.png') }}" sizes="16x16"/>
-        <link rel="icon" type="image/png" href="{{ asset('installer/img/favicon/favicon-32x32.png') }}" sizes="32x32"/>
-        <link rel="icon" type="image/png" href="{{ asset('installer/img/favicon/favicon-96x96.png') }}" sizes="96x96"/>
+        <link rel="icon" type="image" href="{{ asset('installer/img/favicon/favicon.ico') }}"/>
         <link href="{{ asset('installer/css/style.css') }}" rel="stylesheet"/>
         @yield('style')
         <script>
